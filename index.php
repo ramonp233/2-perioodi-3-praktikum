808ed1a53e37a608781821c09c540129d6841316
Ramon <ramonp233@gmail.com>
2014-11-23 19:52:09 +0200

    Finish

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Praktikum 3</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" type"text/css" href="style.css">
+</head>
+<body>
+<form action="form_processing.php" method="post">
+    <span>Kasutajanimi: </span>
+    <input type="text" name="username"><br>
+    <span>Parool: </span>
+    <input type="password" name="password"><br>
+    <input type="submit" name="submit"><br>
+</form>
+</body>
+</html>
